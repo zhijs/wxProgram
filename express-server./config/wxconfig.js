896c50686703c245639436aa0f86d96f4a82089d
zhiyang <2560418273@qq.com>
2018-10-07 15:29:55 +0800

    Update wxconfig.js

--- express-server./config/wxconfig.js
+++ express-server./config/wxconfig.js
@@ -3,8 +3,8 @@
 * 小程序信息
 */
 let wx = {
-  appid: 'wxabb255216735e2b0',
-  secret: '5feeb58d71cfe55a434b0ca0275ba001',
+  appid: '******',
+  secret: '******',
   grant_type: 'authorization_code'
 };
 module.exports = wx;
